15802b50710c7bd65750cfb04f59f952fc53c50e
lordsharkie <babhisek569@gmail.com>
2025-04-13 21:19:40 +0530

    "changed deploy setting from package.json,deleted ./redirects file and removed from vite.config.json."

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,13 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ required for correct JS path resolution on Netlify
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
